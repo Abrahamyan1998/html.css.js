20ffda4ad4d0f4e38ab60618a88e44412c9c5bc1
Abrahamyan1998 <taron.abrahamyan.2018@mail.ru>
2022-11-22 01:18:59 +0400

    html,css,js-click

---
+++ index.js
@@ -0,0 +1,234 @@
+let MODEST = document.querySelector(".mod");
+let SERVICES = document.querySelector(".ser");
+let FACTS = document.querySelector(".fac");
+let PORTFOLIO = document.querySelector(".por");
+let CLIENTS = document.querySelector(".cli");
+let CONTACT = document.querySelector(".con");
+let FOOTER = document.querySelector("footer");
+
+
+
+let modestOpshin = document.querySelector(".modest");
+let servicesOpshin = document.querySelector(".services");
+let factsOpshin = document.querySelector(".facts");
+let portfolioOpshin = document.querySelector(".portfolio");
+let clientsOpshin = document.querySelector(".clients");
+let contactOpshin = document.querySelector(".contact");
+let footerOpshin = modestOpshin;
+
+
+
+
+MODEST.onclick = function () {
+    {
+        modestOpshin.style.display = "block"
+    }
+
+     {
+        servicesOpshin.style.display = "block"
+    }
+
+     {
+        factsOpshin.style.display = "block"
+    }
+
+   {
+        portfolioOpshin.style.display = "block"
+    }
+
+    {
+        clientsOpshin.style.display = "block"
+    }
+
+    {
+        contactOpshin.style.display = "block"
+    }
+
+}
+
+SERVICES.onclick = function () {
+    {
+        modestOpshin.style.display = "none"
+    }
+
+     {
+        servicesOpshin.style.display = "block"
+    }
+
+     {
+        factsOpshin.style.display = "none"
+    }
+
+   {
+        portfolioOpshin.style.display = "none"
+    }
+
+    {
+        clientsOpshin.style.display = "none"
+    }
+
+    {
+        contactOpshin.style.display = "none"
+    }
+
+}
+
+FACTS.onclick = function () {
+    {
+        modestOpshin.style.display = "none"
+    }
+
+     {
+        servicesOpshin.style.display = "none"
+    }
+
+     {
+        factsOpshin.style.display = "block"
+    }
+
+   {
+        portfolioOpshin.style.display = "none"
+    }
+
+    {
+        clientsOpshin.style.display = "none"
+    }
+
+    {
+        contactOpshin.style.display = "none"
+    }
+
+}
+
+PORTFOLIO.onclick = function () {
+    {
+        modestOpshin.style.display = "none"
+    }
+
+     {
+        servicesOpshin.style.display = "none"
+    }
+
+     {
+        factsOpshin.style.display = "none"
+    }
+
+   {
+        portfolioOpshin.style.display = "block"
+    }
+
+    {
+        clientsOpshin.style.display = "none"
+    }
+
+    {
+        contactOpshin.style.display = "none"
+    }
+
+}
+
+CLIENTS.onclick = function () {
+    {
+        modestOpshin.style.display = "none"
+    }
+
+     {
+        servicesOpshin.style.display = "none"
+    }
+
+     {
+        factsOpshin.style.display = "none"
+    }
+
+   {
+        portfolioOpshin.style.display = "none"
+    }
+
+    {
+        clientsOpshin.style.display = "block"
+    }
+
+    {
+        contactOpshin.style.display = "none"
+    }
+
+}
+
+CONTACT.onclick = function () {
+    {
+        modestOpshin.style.display = "none"
+    }
+
+     {
+        servicesOpshin.style.display = "none"
+    }
+
+     {
+        factsOpshin.style.display = "none"
+    }
+
+   {
+        portfolioOpshin.style.display = "none"
+    }
+
+    {
+        clientsOpshin.style.display = "none"
+    }
+
+    {
+        contactOpshin.style.display = "block"
+    }
+
+}
+
+FOOTER.onclick = function () {
+    {
+        modestOpshin.style.display = "block"
+    }
+
+     {
+        servicesOpshin.style.display = "block"
+    }
+
+     {
+        factsOpshin.style.display = "block"
+    }
+
+   {
+        portfolioOpshin.style.display = "block"
+    }
+
+    {
+        clientsOpshin.style.display = "block"
+    }
+
+    {
+        contactOpshin.style.display = "block"
+    }
+
+}
+
+
+
+
+
+
+
+
+
+// let h6 = document.querySelector(".h6")
+// let input = document.querySelectorAll("input")
+// let Name = document.querySelector(".Name")
+
+
+
+// Name.addEventListener('submit', (e) => {
+//     for (let i = 0; i < input.length; i++) {
+//         h6.innerHTML = `<p> ${input[0].value} , ${input[1].value} , ${input[2].value} , ${input[3].value} </p>`
+//     }
+        
+//         e.preventDefault()
+    
+//     })
+
+
